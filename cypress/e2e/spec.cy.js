b1ccee390c1c30fa74cbb8810cf405b23ebb3602
Anil Anar <anilanar@hotmail.com>
2023-02-09 12:10:56 +0100

    add minimal reprod

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,7 @@
 /// <reference types="cypress" />
-describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("page", () => {
+  it("should work", () => {
+    cy.visit("https://34ir1q.csb.app?standalone");
+    cy.get("select[name=select]").select("2");
+  });
+});
